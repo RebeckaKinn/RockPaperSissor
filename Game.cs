717db86d67d04279c29fd9551b6fca8bd4db1aae
RebeckaKinn <rebeckaki@getacademy.no>
2023-04-27 15:08:37 +0200

    changed

--- Game.cs
+++ Game.cs
@@ -7,21 +7,23 @@
             for (var round = 1; round < 11; round++)
             {
                 Console.WriteLine("Round : " + round);
-                int rnd = new Random().Next(players.Count);
-                int rnd2 = rnd + 1;
-                var player1 = players[rnd];
-                var player2 = players[rnd2 == players.Count ? 0 : rnd2];
+                var rnd = new Random();
+                int indexOfPlayer1 = rnd.Next(players.Count);
+                int indexOfPlayer2 = indexOfPlayer1 + 1;
+                var player1 = players[indexOfPlayer1];
+                var player2 = players[indexOfPlayer2 == players.Count ? 0 : indexOfPlayer2];
                 PapirRockSissor(player1, player2);
             }
             CheckWinner(players);
         }
         public void PapirRockSissor(Player player1, Player player2)
         {
-            int nmb = new Random().Next(1, 3);
-            int nmb2 = new Random().Next(1, 3);
-            if (nmb == nmb2) PapirRockSissor(player1, player2);
-            if (nmb == 1 && nmb2 == 2 || nmb == 2 && nmb2 == 3 || nmb == 3 && nmb2 == 1) player1.Win(player2);
-            if (nmb == 2 && nmb2 == 1 || nmb == 3 && nmb2 == 2 || nmb == 1 && nmb2 == 3) player2.Win(player1);
+            var rnd = new Random();
+            int choicePlayer1 = rnd.Next(1, 3);
+            int choicePlayer2 = rnd.Next(1, 3);
+            if (choicePlayer1 == choicePlayer2) PapirRockSissor(player1, player2);
+            if (choicePlayer1 == 1 && choicePlayer2 == 2 || choicePlayer1 == 2 && choicePlayer2 == 3 || choicePlayer1 == 3 && choicePlayer2 == 1) player1.Win(player2);
+            if (choicePlayer1 == 2 && choicePlayer2 == 1 || choicePlayer1 == 3 && choicePlayer2 == 2 || choicePlayer1 == 1 && choicePlayer2 == 3) player2.Win(player1);
         }
         public void CheckWinner(List<Player> players)
         {
